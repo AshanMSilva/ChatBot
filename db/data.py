4cec92b32fa4629aa63c57547257b979abf99022
Ashan M Silva <47840176+AshanMSilva@users.noreply.github.com>
2021-10-27 08:39:06 +0530

    dummy db

---
+++ db/data.py
@@ -0,0 +1,329 @@
+projects ={
+    "1":{
+        "size": 10,
+        "type": "නේවාසික",
+        "no_slots":36,
+        "ava_slots":20,
+        "price_of_perch": 200000,
+        "location":"ගාල්ල",
+        "project":"හරිත උයන"        
+    },
+    "2":{
+        "size": 10,
+        "type": "ව්‍යාපාරික",
+        "no_slots":24,
+        "ava_slots":21,
+        "price_of_perch": 150000,
+        "location":"ගාල්ල",
+        "project":"සිසිල් සෙවණ"        
+    },
+    "3":{
+        "size": 6,
+       "type": "නේවාසික",
+        "no_slots":18,
+        "ava_slots":13,
+        "price_of_perch": 300000,
+        "location":"ගාල්ල",
+        "project":"හයිවේ ගේට්"        
+    },
+    "4":{
+        "size": 12,
+        "type": "ව්‍යාපාරික",
+        "no_slots":52,
+        "ava_slots":39,
+        "price_of_perch": 210000,
+        "location":"ගාල්ල",
+        "project":"ග්‍රීන් සිටි"        
+    },
+    "5":{
+        "size": 6,
+        "type": "නේවාසික",
+        "no_slots":22,
+        "ava_slots":17,
+        "price_of_perch": 450000,
+        "location":"බණ්ඩාරගම",
+        "project":"බණ්ඩාරගම අභිමන්"        
+    },
+    "6":{
+        "size": 10,
+       "type": "ව්‍යාපාරික",
+        "no_slots":24,
+        "ava_slots":8,
+        "price_of_perch": 260000,
+        "location":"බණ්ඩාරගම",
+        "project":"බණ්ඩාරගම ග්‍රීන් සිටි"        
+    },
+    "7":{
+        "size": 40,
+        "type": "නේවාසික",
+        "no_slots":16,
+        "ava_slots":5,
+        "price_of_perch": 70000,
+        "location":"බණ්ඩාරගම",
+        "project":"බණ්ඩාරගම හයිවේ ගේට්"        
+    },
+    "8":{
+        "size": 10,
+        "type": "ව්‍යාපාරික",
+        "no_slots":12,
+        "ava_slots":3,
+        "price_of_perch": 240000,
+        "location":"බණ්ඩාරගම",
+        "project":"හයිවේ සිටි"        
+    },
+    "9":{
+        "size": 15,
+        "type": "නේවාසික",
+        "no_slots":26,
+        "ava_slots":15,
+        "price_of_perch": 270000,
+        "location":"පිළියන්දල",
+        "project":"පිළියන්දල අභිමන්"        
+    },
+    "10":{
+        "size": 8,
+        "type": "ව්‍යාපාරික",
+        "no_slots":20,
+        "ava_slots":20,
+        "price_of_perch": 500000,
+        "location":"පිළියන්දල",
+        "project":"පිළියන්දල හරිත උයන"        
+    },
+    "11":{
+        "size": 12,
+        "type": "නේවාසික",
+        "no_slots":28,
+        "ava_slots":22,
+        "price_of_perch": 320000,
+        "location":"පිළියන්දල",
+        "project":"පිළියන්දල ග්‍රීන් සිටි"        
+    },
+    "12":{
+        "size": 10,
+        "type": "ව්‍යාපාරික",
+        "no_slots":48,
+        "ava_slots":42,
+        "price_of_perch": 260000,
+        "location":"හොරණ",
+        "project":"හොරණ හයිවේ ගේට්"        
+    },
+    "13":{
+        "size": 10,
+        "type": "නේවාසික",
+        "no_slots":18,
+        "ava_slots":3,
+        "price_of_perch": 290000,
+        "location":"හොරණ",
+        "project":"හොරණ සිසිල් සෙවණ"        
+    },
+    "14":{
+        "size": 6,
+        "type": "ව්‍යාපාරික",
+        "no_slots":16,
+        "ava_slots":5,
+        "price_of_perch": 460000,
+        "location":"හොරණ",
+        "project":"හොරණ අභිමන්"        
+    },
+    "15":{
+        "size": 10,
+        "type": "නේවාසික",
+        "no_slots":16,
+        "ava_slots":2,
+        "price_of_perch": 210000,
+        "location":"හොරණ",
+        "project":"හොරණ ග්‍රීන් සිටි"        
+    },
+    "16":{
+        "size": 8,
+        "type": "ව්‍යාපාරික",
+        "no_slots":18,
+        "ava_slots":12,
+        "price_of_perch": 600000,
+        "location":"හොරණ",
+        "project":"හොරණ හරිත උයන"        
+    },
+    "17":{
+        "size": 6,
+        "type": "නේවාසික",
+        "no_slots":12,
+        "ava_slots":5,
+        "price_of_perch": 800000,
+        "location":"නුවර",
+        "project":"නුවර අභිමන්"        
+    },
+    "18":{
+        "size": 15,
+        "type": "ව්‍යාපාරික",
+        "no_slots":16,
+        "ava_slots":3,
+        "price_of_perch": 120000,
+        "location":"නුවර",
+        "project":"නුවර හරිත උයන"        
+    },
+    "19":{
+        "size": 12,
+        "type": "නේවාසික",
+        "no_slots":36,
+        "ava_slots":20,
+        "price_of_perch": 90000,
+        "location":"අම්පාර",
+        "project":"අම්පාර අභිමන්"        
+    },
+    "20":{
+        "size": 10,
+        "type": "ව්‍යාපාරික",
+        "no_slots":16,
+        "ava_slots":8,
+        "price_of_perch": 220000,
+        "location":"අම්පාර",
+        "project":"අම්පාර හරිත උයන"        
+    },
+    "21":{
+        "size": 15,
+        "type": "නේවාසික",
+        "no_slots":42,
+        "ava_slots":25,
+        "price_of_perch": 110000,
+        "location":"මාතර",
+        "project":"මාතර අභිමන්"        
+    },
+    "22":{
+        "size": 10,
+        "type": "ව්‍යාපාරික",
+        "no_slots":112,
+        "ava_slots":79,
+        "price_of_perch": 100000,
+        "location":"මාතර",
+        "project":"මාතර හරිත උයන"        
+    },
+    "23":{
+        "size": 10,
+        "type": "නේවාසික",
+        "no_slots":36,
+        "ava_slots":20,
+        "price_of_perch": 240000,
+        "location":"කුරුණෑගල",
+        "project":"කුරුණෑගල අභිමන්"        
+    },
+    
+
+
+    "24":{
+        "size": 12,
+        "type": "ව්‍යාපාරික",
+        "no_slots":28,
+        "ava_slots":22,
+        "price_of_perch": 320000,
+        "location":"කුරුණෑගල",
+        "project":"කුරුණෑගල හරිත උයන"        
+    },
+    "25":{
+        "size": 10,
+        "type": "නේවාසික",
+        "no_slots":48,
+        "ava_slots":42,
+        "price_of_perch": 260000,
+        "location":"ගම්පහ",
+        "project":"ගම්පහ අභිමන්"        
+    },
+    "26":{
+        "size": 10,
+        "type": "ව්‍යාපාරික",
+        "no_slots":18,
+        "ava_slots":3,
+        "price_of_perch": 290000,
+        "location":"ගම්පහ",
+        "project":"ගම්පහ හරිත උයන"        
+    },
+    "27":{
+        "size": 6,
+        "type": "නේවාසික",
+        "no_slots":16,
+        "ava_slots":5,
+        "price_of_perch": 460000,
+        "location":"ගම්පහ",
+        "project":"ගම්පහ හයිවේ ගේට්"        
+    },
+    "28":{
+        "size": 10,
+        "type": "ව්‍යාපාරික",
+        "no_slots":16,
+        "ava_slots":2,
+        "price_of_perch": 210000,
+        "location":"කළුතර",
+        "project":"කළුතර අභිමන්"        
+    },
+    "29":{
+        "size": 8,
+        "type": "නේවාසික",
+        "no_slots":18,
+        "ava_slots":12,
+        "price_of_perch": 600000,
+        "location":"කළුතර",
+        "project":"කළුතර හයිවේ ගේට්"        
+    },
+    "30":{
+        "size": 6,
+        "type": "ව්‍යාපාරික",
+        "no_slots":12,
+        "ava_slots":5,
+        "price_of_perch": 800000,
+        "location":"කළුතර",
+        "project":"කළුතර හරිත උයන"        
+    },
+    "31":{
+        "size": 15,
+        "type": "නේවාසික",
+        "no_slots":16,
+        "ava_slots":3,
+        "price_of_perch": 120000,
+        "location":"ඉංගිරිය",
+        "project":"ඉංගිරිය අභිමන්"        
+    },
+    "32":{
+        "size": 12,
+        "type": "ව්‍යාපාරික",
+        "no_slots":36,
+        "ava_slots":20,
+        "price_of_perch": 90000,
+        "location":"ඉංගිරිය",
+        "project":"ඉංගිරිය හරිත උයන"        
+    },
+    "33":{
+        "size": 10,
+        "type": "නේවාසික",
+        "no_slots":16,
+        "ava_slots":8,
+        "price_of_perch": 220000,
+        "location":"ඉංගිරිය",
+        "project":"ඉංගිරිය හයිවේ ගේට්"        
+    },
+    "34":{
+        "size": 15,
+        "type": "ව්‍යාපාරික",
+        "no_slots":42,
+        "ava_slots":25,
+        "price_of_perch": 110000,
+        "location":"හෝමාගම",
+        "project":"හෝමාගම අභිමන්"        
+    },
+    "35":{
+        "size": 10,
+        "type": "නේවාසික",
+        "no_slots":112,
+        "ava_slots":79,
+        "price_of_perch": 100000,
+        "location":"හෝමාගම",
+        "project":"හෝමාගම හයිවේ ගේට්"        
+    },
+    "36":{
+        "size": 10,
+        "type": "ව්‍යාපාරික",
+        "no_slots":36,
+        "ava_slots":20,
+        "price_of_perch": 240000,
+        "location":"මහරගම",
+        "project":"මහරගම අභිමන්"       
+    },
+}
